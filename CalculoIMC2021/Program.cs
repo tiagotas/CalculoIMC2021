d3042a571a254ec584db807dde8f0cfb9573998a
TIAGO SILVA <frost.tiago@gmail.com>
2021-02-25 21:54:39 -0300

    Alterações no ToString

--- CalculoIMC2021/Program.cs
+++ CalculoIMC2021/Program.cs
@@ -6,6 +6,35 @@ namespace CalculoIMC2021
     {
         static void Main(string[] args)
         {
+
+            // Operadores
+            //   + de adição (observar o contexto)
+            //   - de subtração
+            //   * de multiplicação
+            //   / de divisão
+            //   % de módulo, pega o resto de uma divisão
+
+
+            /*int numero;
+
+            Console.WriteLine("Digite um numero inteiro qualquer: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            double resultado_divisao_por_dois;
+
+            resultado_divisao_por_dois = numero / 2;
+
+            Console.WriteLine("O resultado da divisão é {0} ", resultado_divisao_por_dois);
+
+            double resto_da_divisao = numero % 2;
+
+            Console.WriteLine("O resto da divisao do número por dois é {0}", resto_da_divisao);
+            */
+
+
+
+
+
             /**
              * imc = peso / (altura * altura)
              */
@@ -26,7 +55,9 @@ namespace CalculoIMC2021
             // Realizando o cálculo do IMC
             imc = peso / (altura * altura);
 
-            Console.WriteLine("Seu IMC é: " + imc);
+
+                                                  // Arredondamento das casas decimais.
+            Console.WriteLine("Seu IMC é: " + imc.ToString("0.00"));
 
             /**
              * Regras de Classificação do IMC
@@ -36,7 +67,7 @@ namespace CalculoIMC2021
              * IMC entre 30,0 e 34,9 = "Obesidade Grau I"
              * IMC entre 35,0 e 39,9 = "Obesidade Grau II"
              * IMC maior que 40 = "Obesidade Grau III ou Mórbida"
-             */ 
+             */
 
             // SE = if
             // SENÃO = else
@@ -44,24 +75,29 @@ namespace CalculoIMC2021
             if (imc < 18.5)
             {
                 Console.WriteLine("Muito abaixo do peso.");
-                                // E
-            } else if (imc >= 18.5 && imc <= 24.9)
+                // E
+            }
+            else if (imc >= 18.5 && imc <= 24.9)
             {
                 Console.WriteLine("Peso normal.");
 
-            } else if (imc >= 25 && imc <= 29.9)
+            }
+            else if (imc >= 25 && imc <= 29.9)
             {
                 Console.WriteLine("Você está com sobrepeso.");
 
-            } else if (imc >= 30 && imc <= 34.9)
+            }
+            else if (imc >= 30 && imc <= 34.9)
             {
                 Console.WriteLine("Você está com Obesidade Grau I");
 
-            } else if(imc >= 35 && imc <= 39.9)
+            }
+            else if (imc >= 35 && imc <= 39.9)
             {
                 Console.WriteLine("Você está com Obesidade Grau II");
 
-            } else 
+            }
+            else
             {
                 Console.WriteLine("Você está com Obesidade Grau III");
             }
@@ -73,7 +109,7 @@ namespace CalculoIMC2021
 
 
             Console.ReadKey();
-            
+
         }
     }
 }
